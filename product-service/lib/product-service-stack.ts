4977f5efc5b3cd9a3780f430788bcf7fda8f73e3
Anar Hasanov <anar.hasanov79@gmail.com>
2024-06-15 09:13:31 +0400

    add api gateway config

--- product-service/lib/product-service-stack.ts
+++ product-service/lib/product-service-stack.ts
@@ -1,16 +1,55 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Lambda function for getting the list of products
+    const getProductsListLamFn = new lambda.Function(
+      this,
+      "getProductsListLamFn",
+      {
+        runtime: lambda.Runtime.NODEJS_20_X,
+        handler: "getProductsList.handler",
+        code: lambda.Code.fromAsset("lambda"),
+      }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ProductServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda function for getting a product by ID
+    const getProductsByIdLamFn = new lambda.Function(
+      this,
+      "getProductsByIdLamFn",
+      {
+        runtime: lambda.Runtime.NODEJS_20_X,
+        handler: "getProductsById.handler",
+        code: lambda.Code.fromAsset("lambda"),
+      }
+    );
+
+    // Create an API Gateway
+    const api = new apigateway.RestApi(this, "productsApi", {
+      restApiName: "Products Service",
+    });
+
+    // Define the /products resource
+    const products = api.root.addResource("products");
+
+    // Integrate the GET method with the Lambda function for listing products
+    products.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(getProductsListLamFn)
+    );
+
+    // Define the /products/{productId} resource
+    const productById = products.addResource("{productId}");
+
+    // Integrate the GET method with the Lambda function for getting a product by ID
+    productById.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(getProductsByIdLamFn)
+    );
   }
 }
