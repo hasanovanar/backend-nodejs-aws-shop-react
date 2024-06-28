4199b9ea2950cf5ae67f18e12fa82dac6de265e6
Anar Hasanov <anar.hasanov79@gmail.com>
2024-06-28 11:07:08 +0400

    feat: configure stacek for lambdas

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,16 +1,59 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3Notifications from "aws-cdk-lib/aws-s3-notifications";
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Reference to the existing S3 bucket
+    const bucket = s3.Bucket.fromBucketName(
+      this,
+      "ExistingBucket",
+      "import-products-service"
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create Lambda functions
+    const importProductsFile = new lambda.Function(this, "importProductsFile", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "importProductsFile.handler",
+      code: lambda.Code.fromAsset("lambda"),
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      },
+    });
+
+    const importFileParser = new lambda.Function(this, "importFileParser", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "importFileParser.handler",
+      code: lambda.Code.fromAsset("lambda"),
+    });
+
+    // Grant permissions to the importProductsFile Lambda function
+    bucket.grantReadWrite(importProductsFile);
+
+    // Create an API Gateway
+    const api = new apigateway.RestApi(this, "importApi", {
+      restApiName: "Import Service",
+    });
+
+    const importProd = api.root.addResource("import");
+    importProd.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(importProductsFile)
+    );
+
+    // Set up S3 event notification to trigger importFileParser Lambda function
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3Notifications.LambdaDestination(importFileParser),
+      { prefix: "uploaded/" }
+    );
+
+    // Grant S3 read permissions to importFileParser Lambda function
+    bucket.grantRead(importFileParser);
   }
 }
