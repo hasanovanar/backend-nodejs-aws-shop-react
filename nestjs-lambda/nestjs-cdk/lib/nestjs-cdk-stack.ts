5b11bc4f1db19d40acb27ace8450a190ac49183f
Anar Hasanov <anar.hasanov79@gmail.com>
2024-07-20 10:43:29 +0400

    feat: change stack file

--- nestjs-lambda/nestjs-cdk/lib/nestjs-cdk-stack.ts
+++ nestjs-lambda/nestjs-cdk/lib/nestjs-cdk-stack.ts
@@ -1,16 +1,30 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class NestjsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define the Lambda function
+    const nestLambda = new lambda.Function(this, "NestLambda", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "lambda.handler",
+      code: lambda.Code.fromAsset("../nodejs-aws-cart-api/dist"),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'NestjsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Define API Gateway to trigger the Lambda function
+    const api = new apigateway.RestApi(this, "Api", {
+      deployOptions: {
+        stageName: "prod",
+      },
+    });
+
+    api.root.addProxy({
+      defaultIntegration: new apigateway.LambdaIntegration(nestLambda, {
+        proxy: true,
+      }),
+    });
   }
 }
